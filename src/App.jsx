84b62bcc9244ed4db0541c00d00e8b49cc4ccbe5
darq <prototypid@gmail.com>
2024-11-04 14:07:14 +0530

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import { useState } from 'react';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <h2 className='text-emerald-500'>Hello there</h2>;
+}
+
+export default App;
