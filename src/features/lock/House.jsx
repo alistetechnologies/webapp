4ee8dbdc433b01e773bb7c9d9c86ce52691a721a
JanhaviTomar <99262785+JanhaviTomar@users.noreply.github.com>
2025-02-15 12:36:41 +0530

    view details update

--- src/features/lock/House.jsx
+++ src/features/lock/House.jsx
@@ -8,21 +8,23 @@ import { useUser } from '../auth/api/userStore'
 export default function House({house,index}) {
    const navigate=useNavigate()
    const user = useUser.getState().user
+   const [showHouseDetails, setShowHouseDetails] = useState(false);
   
   return (
     <TableRow className='text-lg'>
         <TableCell>{index+1}</TableCell>
         <TableCell>{house?.label}</TableCell>
         <TableCell>
-          <Button
-            onClick={() => {
-            useUser.getState().updateUser({...user,selectedHouse:house?.value});
-               navigate(`/lock/house`)
-            }}
-          >
-            View Details
-          </Button>
-        </TableCell>
+  <Button
+    onClick={() => {
+      setShowHouseDetails((prev) => !prev);
+      useUser.getState().updateUser({ ...user, selectedHouse: house?.value });
+      navigate(`/lock/house`);
+    }}
+  >
+    {showHouseDetails ? "Hide Details" : "View Details"}
+  </Button>
+</TableCell>
       </TableRow>
   )
 }
