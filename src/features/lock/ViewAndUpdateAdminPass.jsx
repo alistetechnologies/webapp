4ba48942871f42e56a139194c388ea648b52fde0
harshbhar0629 <harshbhar.0629@gmail.com>
2025-04-10 10:02:07 +0530

    fix:

--- src/features/lock/ViewAndUpdateAdminPass.jsx
+++ src/features/lock/ViewAndUpdateAdminPass.jsx
@@ -6,6 +6,8 @@ import toast from "react-hot-toast";
 import { adminPasswordApi } from "./api";
 import { token } from "@/constants/config";
 import { Spinner } from "react-bootstrap";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
+import "./viewAndUpdate.css"
 
 export default function ViewAndUpdateAdminPass({
 	showModal,
@@ -18,6 +20,8 @@ export default function ViewAndUpdateAdminPass({
 	const [confirmPassword, setConfirmPassword] = useState("");
 	const [error, setError] = useState("");
 	const [loading, setLoading] = useState(false);
+	const [show, setShow] = useState(false);
+	const [showPassword, setShowPassword] = useState(false);
 
 	const handleSubmit = async () => {
 		if (newPassword !== confirmPassword) {
@@ -104,39 +108,39 @@ export default function ViewAndUpdateAdminPass({
 							</>
 						) : (
 							<>
-								<div className="mb-3">
+								<div className="mb-3 relative">
 									<label className="block text-sm font-medium mb-1">
-										Enter New Password
+										New Password
 									</label>
 									<input
-										type="password"
-										name="newPassword"
-										autocomplete="new-password"
-										readonly
-										onfocus="this.removeAttribute('readonly');"
+										type={show ? "text" : "password"}
 										value={newPassword}
 										onChange={(e) => {
 											if (e.target.value[0] === "0") {
 												setError("Password can't start with 0");
 												return;
-											}
-											else {
+											} else {
 												setError("");
 											}
 											setNewPassword(e.target.value);
 										}}
-										className="w-full px-3 py-2 border rounded"
+										className="w-full px-3 py-2 pr-10 border rounded"
 									/>
+									<span
+										className="absolute right-3 top-8 cursor-pointer text-gray-600"
+										onClick={() => setShow(!show)}>
+										{show ? <VisibilityOff /> : <Visibility />}
+									</span>
 								</div>
 
-								<div className="mb-3">
+								<div className="mb-3 relative">
 									<label className="block text-sm font-medium mb-1">
 										Confirm Password
 									</label>
 									<input
-										type="password"
+										type={showPassword ? "text" : "password"}
 										value={confirmPassword}
-										onChange={(e) =>{
+										onChange={(e) => {
 											if (e.target.value[0] === "0") {
 												setError("Password can't start with 0");
 												return;
@@ -145,13 +149,29 @@ export default function ViewAndUpdateAdminPass({
 											}
 											setConfirmPassword(e.target.value);
 										}}
-										className="w-full px-3 py-2 border rounded"
+										className="w-full px-3 py-2 pr-10 border rounded"
 									/>
+									<span
+										className="absolute right-3 top-8 cursor-pointer text-gray-600"
+										onClick={() => setShowPassword(!showPassword)}>
+										{showPassword ? <VisibilityOff /> : <Visibility />}
+									</span>
 								</div>
 
 								{error && <p className="text-red-500 text-sm mb-2">{error}</p>}
 								{loading && <Spinner className="w-5 h-5 text-blue-500" />}
 								<div className="flex justify-end space-x-2">
+									<Button
+										className="cursor-pointer"
+										disabled={loading}
+										onClick={() => {
+											const pass = Math.floor(Math.random() * 1e6);
+											console.log(pass);
+											setNewPassword(pass);
+											setConfirmPassword(pass);
+										}}>
+										Generate Password
+									</Button>
 									<Button
 										className="cursor-pointer"
 										disabled={loading}
