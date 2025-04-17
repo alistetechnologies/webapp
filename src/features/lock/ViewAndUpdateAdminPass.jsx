395ea4f8630ddcb51f249fc97f4c0dab30833c0b
harshbhar0629 <harshbhar.0629@gmail.com>
2025-04-17 15:24:34 +0530

    fix:

--- src/features/lock/ViewAndUpdateAdminPass.jsx
+++ src/features/lock/ViewAndUpdateAdminPass.jsx
@@ -6,7 +6,7 @@ import toast from "react-hot-toast";
 import { adminPasswordApi } from "./api";
 import { token } from "@/constants/config";
 import { Spinner } from "react-bootstrap";
-import { Visibility, VisibilityOff } from "@mui/icons-material";
+import { ArrowBack, Visibility, VisibilityOff } from "@mui/icons-material";
 import "./viewAndUpdate.css"
 
 export default function ViewAndUpdateAdminPass({
@@ -78,9 +78,22 @@ export default function ViewAndUpdateAdminPass({
 					<div className="fixed inset-0 bg-black/50 z-40"></div>
 
 					<div className="fixed left-1/2 top-1/2 z-50 w-full max-w-md -translate-x-1/2 -translate-y-1/2 bg-white rounded-xl p-6 shadow-lg">
-						<h2 className="text-xl font-semibold mb-4">
-							Admin Password Settings
-						</h2>
+						<div className="cursor-pointer flex items-center gap-[4px]">
+							{editMode && <div
+								disabled={loading}
+								onClick={() => {
+									setEditMode(false);
+									setNewPassword("");
+									setConfirmPassword("");
+									setError("");
+								}}
+								className="mb-[20px]">
+								<ArrowBack />
+							</div>}
+							<h2 className="text-xl font-semibold mb-4">
+								Admin Password Settings
+							</h2>
+						</div>
 
 						{!editMode ? (
 							<>
@@ -160,13 +173,17 @@ export default function ViewAndUpdateAdminPass({
 
 								{error && <p className="text-red-500 text-sm mb-2">{error}</p>}
 								{loading && <Spinner className="w-5 h-5 text-blue-500" />}
-								<div className="flex justify-end space-x-2">
+								<div className="flex justify-between space-x-2">
 									<Button
 										className="cursor-pointer"
 										disabled={loading}
 										onClick={() => {
 											let pass = Math.floor(Math.random() * 1e6);
+											pass = String(pass);
 											console.log(pass);
+											if (pass.length != 6) {
+												return;
+											}
 											setNewPassword(String(pass));
 											setConfirmPassword(String(pass));
 										}}>
@@ -177,18 +194,6 @@ export default function ViewAndUpdateAdminPass({
 										disabled={loading}
 										onClick={handleSubmit}>
 										Submit
-									</Button>
-									<Button
-										variant="error"
-										disabled={loading}
-										onClick={() => {
-											setEditMode(false);
-											setNewPassword("");
-											setConfirmPassword("");
-											setError("");
-										}}
-										className="bg-red-600 hover:bg-red-700 text-white cursor-pointer">
-										Back
 									</Button>
 								</div>
 							</>
