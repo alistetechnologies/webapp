d52bc1c302f4835ca2a180f6befaecfaffe5b284
darq <prototypid@gmail.com>
2025-05-16 00:53:44 +0530

    fix(Dashboard): Control being called even if device is offline

--- src/features/dashboard/appliance-details.jsx
+++ src/features/dashboard/appliance-details.jsx
@@ -1,12 +1,12 @@
-import { TableCell, TableRow } from "@/components/ui/table";
-import { convertMilliseconds } from "@/utils/format";
-import { Dot } from "lucide-react";
-import { controlDevice } from "./api/device";
-import { useEffect, useState } from "react";
-import { Spinner } from "@/components/ui/spinner";
-import toast from "react-hot-toast";
-import { useUser } from "../auth/api/userStore";
-import useHouseStore from "./houseStore";
+import { TableCell, TableRow } from '@/components/ui/table';
+import { convertMilliseconds } from '@/utils/format';
+import { Dot } from 'lucide-react';
+import { controlDevice } from './api/device';
+import { useEffect, useState } from 'react';
+import { Spinner } from '@/components/ui/spinner';
+import toast from 'react-hot-toast';
+import { useUser } from '../auth/api/userStore';
+import useHouseStore from './houseStore';
 
 export function ApplianceDetails({
   appliances,
@@ -38,15 +38,20 @@ export function ApplianceDetails({
     );
   }, [appliances]);
   const switchCommands = analysisData?.controlLogs.filter(
-    (cmd) => cmd?.controllerType === "switch"
+    (cmd) => cmd?.controllerType === 'switch'
   ).length;
   const smartCommands = analysisData?.controlLogs.filter(
-    (cmd) => cmd?.controllerType !== "switch"
+    (cmd) => cmd?.controllerType !== 'switch'
   ).length;
 
-  const toggleApplianceState = async () => {
+  const toggleApplianceState = async (deviceConnected) => {
+    if (!deviceConnected) {
+      toast.error('Device is offline');
+      return;
+    }
+
     setLoading(true);
-    const command = String(appliance.switchState) === "0" ? "100" : "0";
+    const command = String(appliance.switchState) === '0' ? '100' : '0';
     const resp = await controlDevice({
       command,
       deviceId: appliance.deviceId,
@@ -64,51 +69,62 @@ export function ApplianceDetails({
     // }
   };
 
+  const deviceConnected = connectedDevices.some(
+    (d) => d.deviceId === analysisData?.deviceId
+  );
+
   return (
-    <TableRow className="">
+    <TableRow className=''>
       <TableCell>{sno}</TableCell>
-      <TableCell className="text-lg text-black hover:underline">
+      <TableCell className='text-lg text-black hover:underline'>
         {appliance?.switchName}
       </TableCell>
-      <TableCell className="text-lg text-center">
+      <TableCell className='text-lg text-center'>
         {onTime?.hours && onTime?.seconds && onTime?.minutes
-          ? `${String(onTime?.hours).padStart(2, "0")} hr ${String(
+          ? `${String(onTime?.hours).padStart(2, '0')} hr ${String(
               onTime?.minutes
-            ).padStart(2, "0")} min`
-          : "--"}
+            ).padStart(2, '0')} min`
+          : '--'}
       </TableCell>
-      <TableCell className="text-lg text-center">
+      <TableCell className='text-lg text-center'>
         {onTime?.hours && onTime?.seconds && onTime?.minutes
-          ? `${String(onlineTime?.hours).padStart(2, "0")} hr ${String(
+          ? `${String(onlineTime?.hours).padStart(2, '0')} hr ${String(
               onlineTime?.minutes
-            ).padStart(2, "0")} min`
-          : "--"}
+            ).padStart(2, '0')} min`
+          : '--'}
       </TableCell>
-      <TableCell className="text-lg text-center">
-        {analysisData?.controlLogs?.length || "-"}
+      <TableCell className='text-lg text-center'>
+        {analysisData?.controlLogs?.length || '-'}
       </TableCell>
-      <TableCell className="text-lg text-center">
-        {smartCommands || "-"}
+      <TableCell className='text-lg text-center'>
+        {smartCommands || '-'}
       </TableCell>
-      <TableCell className="font-bold text-center">
-        {switchCommands || "-"}
+      <TableCell className='font-bold text-center'>
+        {switchCommands || '-'}
       </TableCell>
-      <TableCell
-        className="cursor-pointer text-lg"
-        onClick={toggleApplianceState}
-      >
-        {appliance?.switchState === "0" ? (
-          <div className="p-1 bg-red-500/10 flex items-center justify-center rounded-md px-4 text-red-500 w-20">
+      <TableCell className='cursor-pointer text-lg'>
+        {appliance?.switchState === '0' ? (
+          <div
+            className='p-1 bg-red-500/10 flex items-center justify-center rounded-md px-4 text-red-500 w-20'
+            onClick={() => {
+              toggleApplianceState(deviceConnected);
+            }}
+          >
             {loading ? (
               <Spinner />
             ) : (
               <>
-                <Dot /> Off{" "}
+                <Dot /> Off{' '}
               </>
             )}
           </div>
         ) : (
-          <div className="p-1 bg-green-500/10 flex items-center justify-center rounded-md text-green-500 w-20">
+          <div
+            className='p-1 bg-green-500/10 flex items-center justify-center rounded-md text-green-500 w-20'
+            onClick={() => {
+              toggleApplianceState(deviceConnected);
+            }}
+          >
             {loading ? (
               <Spinner />
             ) : (
@@ -120,13 +136,13 @@ export function ApplianceDetails({
         )}
       </TableCell>
 
-      <TableCell className="cursor-pointer text-lg font-semibold">
-        {connectedDevices.some((d) => d.deviceId === analysisData?.deviceId) ? (
-          <div className=" flex items-center justify-center rounded-md px-4 text-green-500">
+      <TableCell className='cursor-pointer text-lg font-semibold'>
+        {deviceConnected ? (
+          <div className=' flex items-center justify-center rounded-md px-4 text-green-500'>
             Online
           </div>
         ) : (
-          <div className="p-2 flex items-center justify-center rounded-md px-4 text-red-500">
+          <div className='p-2 flex items-center justify-center rounded-md px-4 text-red-500'>
             Offline
           </div>
         )}
