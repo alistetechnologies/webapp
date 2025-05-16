ab24016876b004e4dfb2d3b24292697b8be79621
sahil <prototypid@gmail.com>
2025-05-16 12:32:03 +0530

    fix(Shareaccess): guest access date cannot be less than todays

--- src/features/shareAccess/AddUser.jsx
+++ src/features/shareAccess/AddUser.jsx
@@ -324,6 +324,7 @@ const AddUser = ({
                   <input
                     type="date"
                     value={validTill}
+                    min={moment().format("YYYY-MM-DD")}
                     onChange={handleDateChange}
                     className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"
                   />
