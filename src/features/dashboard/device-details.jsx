c4a5cc2d045ebbd3cef93005620734cbc0e99ccc
sahil <prototypid@gmail.com>
2025-06-12 15:39:34 +0530

    feat(Dashboard): Show on devices

--- src/features/dashboard/device-details.jsx
+++ src/features/dashboard/device-details.jsx
@@ -132,7 +132,9 @@ export function DeviceDetails({
             <span className="text-red-400">No</span>
           )}
         </TableCell>
-        <TableCell>{totalAppliances}</TableCell>
+        <TableCell>
+          {onAppliances}/{totalAppliances}
+        </TableCell>
         <TableCell>
           {totalAppliances === 0 ? (
             <span className="text-red-600">0 %</span>
