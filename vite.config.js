5804d1c2407ab28a9bfbcb8827348320d747179c
Akhil161 <42910002+Akhil161@users.noreply.github.com>
2025-11-08 14:36:57 +0530

    add

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
+import {resolve} from "path";
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        octiot: resolve(__dirname, 'index-octiot.html'),
+      },
+    },
+  },
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src"),
+      "@": resolve(__dirname, "src"),
     },
   },
 });
