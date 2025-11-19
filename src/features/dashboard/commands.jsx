e8a50d4753beebad70ca0e22495750f17b891bb9
sahil <prototypid@gmail.com>
2025-11-19 19:03:05 +0530

    fixed

--- src/features/dashboard/commands.jsx
+++ src/features/dashboard/commands.jsx
@@ -3,23 +3,9 @@ import { octiotFont } from "@/constants/config";
 import { isOctiot } from "@/utils/browser";
 import React, { useEffect, useState } from "react";
 
-export default function Commands({ analysisData }) {
+export default function Commands({ analysisData, logsDate }) {
   const [smartCommands, setSmartCommands] = useState(0);
   const [switchCommands, setSwitchCommands] = useState(0);
-
-  function calculateCommands() {
-    let smartCommands = 0;
-    let switchCommands = 0;
-    for (const device of analysisData) {
-      for (const s of device) {
-        switchCommands += s?.toggles?.switch || 0;
-        smartCommands += s?.toggles?.aliste || 0;
-      }
-    }
-
-    setSmartCommands(smartCommands);
-    setSwitchCommands(switchCommands);
-  }
 
   useEffect(() => {
     if (analysisData?.length === 0 || !analysisData) return;
@@ -29,12 +15,24 @@ export default function Commands({ analysisData }) {
       let totalSwitchCommands = 0;
 
       for (const device of analysisData) {
+        const controlLogs = (device?.controlLogs || []).filter((log) => {
+          const timestamp = log.originalTimestamp || log.timestamp;
+          const logDateTimestamp = new Date(logsDate).setHours(0, 0, 0, 0);
+          if (
+            log.controllerId === "disconnect" ||
+            log.controllerId === "conn" ||
+            log.controllerType === "disconnect"
+          )
+            return false;
+          if (isNaN(timestamp) || isNaN(logDateTimestamp)) return false;
+          return timestamp >= logDateTimestamp;
+        });
         if (device.applianceType === 7) continue;
         if (device) {
-          const switchCommands = device?.controlLogs.filter(
+          const switchCommands = controlLogs.filter(
             (cmd) => cmd?.controllerType === "switch"
           ).length;
-          const smartCommands = device?.controlLogs.filter(
+          const smartCommands = controlLogs.filter(
             (cmd) => cmd?.controllerType !== "switch"
           ).length;
 
@@ -50,9 +48,27 @@ export default function Commands({ analysisData }) {
   }, [analysisData]);
   return (
     <>
-      <TableCell style={{...(isOctiot ? {fontSize:octiotFont.subHeaderFontSize}:{})}}>{smartCommands + switchCommands}</TableCell>
-      <TableCell style={{...(isOctiot ? {fontSize:octiotFont.subHeaderFontSize}:{})}}>{smartCommands}</TableCell>
-      <TableCell style={{...(isOctiot ? {fontSize:octiotFont.subHeaderFontSize}:{})}}>{switchCommands}</TableCell>
+      <TableCell
+        style={{
+          ...(isOctiot ? { fontSize: octiotFont.subHeaderFontSize } : {}),
+        }}
+      >
+        {smartCommands + switchCommands}
+      </TableCell>
+      <TableCell
+        style={{
+          ...(isOctiot ? { fontSize: octiotFont.subHeaderFontSize } : {}),
+        }}
+      >
+        {smartCommands}
+      </TableCell>
+      <TableCell
+        style={{
+          ...(isOctiot ? { fontSize: octiotFont.subHeaderFontSize } : {}),
+        }}
+      >
+        {switchCommands}
+      </TableCell>
     </>
   );
   return (
