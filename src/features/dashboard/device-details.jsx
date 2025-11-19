067729821b83b8c9b4f16800bcfdd8a031ad2429
sahil <prototypid@gmail.com>
2025-11-19 18:31:56 +0530

    Devcie commands fix

--- src/features/dashboard/device-details.jsx
+++ src/features/dashboard/device-details.jsx
@@ -200,7 +200,7 @@ export function DeviceDetails({
             </span>
           )}
         </TableCell>
-        <Commands analysisData={appliancesAnalysisData} />
+        <Commands analysisData={appliancesAnalysisData} logsDate={date} />
         <TableCell
           className="flex justify-between gap-2"
           style={{
